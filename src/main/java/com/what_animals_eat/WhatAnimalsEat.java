5ad0f282cb662f6e3a0ffd8ee8b4f30f8ec8eb29
qlnx2012 <qlnx2012@user.noreply.gitee.com>
2026-08-27 17:15:43 +0800

    Use pretty JSON breeding food config

--- src/main/java/com/what_animals_eat/WhatAnimalsEat.java
+++ src/main/java/com/what_animals_eat/WhatAnimalsEat.java
@@ -5,7 +5,6 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.config.ModConfig;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
 import org.slf4j.Logger;
@@ -16,7 +15,6 @@ public final class WhatAnimalsEat {
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public WhatAnimalsEat(IEventBus modEventBus, ModContainer modContainer) {
-        modContainer.registerConfig(ModConfig.Type.COMMON, WhatAnimalsEatConfig.SPEC);
         NeoForge.EVENT_BUS.register(new BreedingFoodHandler());
         NeoForge.EVENT_BUS.register(this);
     }
